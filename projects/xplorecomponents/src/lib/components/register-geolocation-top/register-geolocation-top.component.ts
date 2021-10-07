7d05ae1a22ee102c89ad28196370ec62cca60651
Jose Antonio Marin Lopez <joseantonio.marinlopez@xploregroup.es>
2021-10-07 09:23:22 +0200

    Get data from component.data

--- projects/xplorecomponents/src/lib/components/register-geolocation-top/register-geolocation-top.component.ts
+++ projects/xplorecomponents/src/lib/components/register-geolocation-top/register-geolocation-top.component.ts
@@ -1,61 +1,66 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CmsComponentData } from '@spartacus/storefront';
 import { Observable } from 'rxjs';
 import { RegisterGeolocationTopModel } from './register-geolocation-top.model';
 import { RegisterGeolocationTopStore } from './register-geolocation-top.store';
 
 @Component({
-    selector: 'register-geolocation-top',
-    templateUrl: './register-geolocation-top.component.html',
-    styleUrls: ['./register-geolocation-top.scss'],
-    animations: [
-      trigger('hideMsg', [
-        state('hide', style({
-          opacity: 0
+  selector: 'register-geolocation-top',
+  templateUrl: './register-geolocation-top.component.html',
+  styleUrls: ['./register-geolocation-top.scss'],
+  animations: [
+    trigger('hideMsg', [
+      state('hide', style({
+        opacity: 0
       })),
       state('show', style({
-          opacity: 1
+        opacity: 1
       })),
-        transition('* => *', animate(300))
-      ]),
-    ],
-    providers: [RegisterGeolocationTopStore]
-  })
+      transition('* => *', animate(300))
+    ]),
+  ],
+  providers: [RegisterGeolocationTopStore]
+})
 
-  export class RegisterGeolocationTopComponent implements OnInit{
+export class RegisterGeolocationTopComponent implements OnInit {
 
   constructor(
     public component: CmsComponentData<RegisterGeolocationTopModel>,
     public store: RegisterGeolocationTopStore
-  ) {}
+  ) { }
 
   ngOnInit() {
-    if(this.component?.data$) {
+    if (this.component?.data$) {
       this.component.data$.subscribe(data => {
-        // Initialize state store
+        this.store.setState({
+          ...this.store.state$,
+          currentCountry: data.country,
+          message: data.content
+        })
       })
     }
   }
-    get stateName() {
-      console.log(`Is hide: ${this.getValue(this.store.isHide)}`)
-      return this.getValue(this.store.isHide) ? 'hide' : 'show';
-    }
-  
-    closeMsg() {
-      this.store.setHide(!this.getValue(this.store.isHide))
-    }
-  
-    changeLocation(country: string | undefined) {
-      this.closeMsg();
-      this.store.setSelectedCountry('Country changed to ' + country);
-    }
 
-    getValue(obj: Observable<any>){
-      let value: any;
-      obj.subscribe(v => value = v);
-      return value;
-    }
+  get stateName() {
+    console.log(`Is hide: ${this.getValue(this.store.isHide)}`)
+    return this.getValue(this.store.isHide) ? 'hide' : 'show';
   }
+
+  closeMsg() {
+    this.store.setHide(!this.getValue(this.store.isHide))
+  }
+
+  changeLocation(country: string | undefined) {
+    this.closeMsg();
+    this.store.setSelectedCountry('Country changed to ' + country);
+  }
+
+  getValue(obj: Observable<any>) {
+    let value: any;
+    obj.subscribe(v => value = v);
+    return value;
+  }
+}
 
